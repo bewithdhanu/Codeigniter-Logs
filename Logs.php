ed7193a9b1a276431b37214ffaf6f999042afb33
Dhanu K Parvateesam <bewithdhanu@gmail.com>
2018-12-19 16:12:15 +0530

    Update Logs.php

--- Logs.php
+++ Logs.php
@@ -5,7 +5,7 @@ Author:Dhanu K
 Email:bewithdhanu@gmail.com
 Website:bewithdhanu.in
 *******/
-class logs extends CI_Controller
+class Logs extends CI_Controller
 {
 
     public function __construct()
